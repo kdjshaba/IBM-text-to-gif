58c6b06645c240f9641ae14369db07b589cc1857
tadasbal <109873910+tadasbal@users.noreply.github.com>
2023-02-02 09:10:40 +0200

    Delete test.js

--- backend/modules/test.js
+++
@@ -1,8 +0,0 @@
-const watson = require('./watson-nlu')
-
-const testuoju = async (text) => {
-    const category = await watson.textCategory(text);
-    console.log (category)
-}
-
-testuoju('I love computers, I am very good at programming')
